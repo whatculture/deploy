14bd2a5a0a131495f82a8ddf0f88c1f166efd869
Kieron <info@kieronwilson.co.uk>
2016-07-27 11:22:51 +0100

    updates

--- console.py
+++ console.py
@@ -10,13 +10,13 @@ class Console:
 		sys.stdout.write(message.strip() + "\n")
 
 	def success(self, message):
-		self.message("\e[32m--> \e[39m" + message)
+		self.message('--> ' + message)
 
 	def error(self, message):
-		self.message("\e[31merror: \e[39m" + message)
+		self.message('error: ' + message)
 
 	def run(self, args, cwd=None):
 		self.message('command --> ' + ' '.join(args))
 		proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd)
 		stdout, stderr = proc.communicate()
-		return stderr
+		return stdout
